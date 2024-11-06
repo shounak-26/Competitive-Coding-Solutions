dac324b80faca80e08859ebb0bd1d8754f47cc0b
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-11-06 14:54:00 +0530

    Time: 5 ms (61.96%), Space: 68.7 MB (30.88%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if(head == null || head.next == null) return true;
+        ListNode currNode = head;
+        ListNode prevNode = null;
+        int loop = findLengthOfLinkedList(currNode);
+        for(int i=0; i<=loop/2-1; i++){
+            currNode = currNode.next;
+        }
+        while(currNode != null){
+            ListNode nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        
+        ListNode startNode = head;
+        boolean flag = false;
+        while(startNode != null  && prevNode != null){
+            if(startNode.val != prevNode.val){
+                flag = false;
+                break;
+            }else{
+                flag = true;
+                startNode = startNode.next;
+                prevNode = prevNode.next;
+            }
+        }
+        // if((startNode.next == null || prevNode.next == null) && (startNode.val == prevNode.val)){
+        //         flag = true;
+        // }
+        if(flag == true){
+           return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int findLengthOfLinkedList(ListNode currNode){
+        int length = 0;
+        while(currNode != null){
+            length++;
+            currNode = currNode.next;
+        }
+        return length;
+    }
+}
