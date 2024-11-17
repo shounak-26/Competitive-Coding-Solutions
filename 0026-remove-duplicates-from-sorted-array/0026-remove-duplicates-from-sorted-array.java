81cb785d4825ae50dea5c2bc7cd3cf46f1de97d1
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-11-17 22:06:05 +0530

    Time: 1 ms (67.16%), Space: 44.4 MB (89.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,42 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        while(i < nums.length - 1){
-            if(nums[i] == nums[i+1]){
-                nums[i] = Integer.MAX_VALUE;
-                i++;
-            }
-            else{
-                i++;
-            }
+    int count = 0;
+    int n = nums.length - 1;
+    int ptr = 0;
+    for(int i = 1; i <= n; i++){
+        if(nums[i] == nums[i-1] ){
+            count++;
+        }else{
+        nums[ptr] = nums[i-1];
+          ptr++;
         }
-
-        bubbleSort(nums);
-        int count = 0;
-        for(int j : nums ){
-            
-            if(j != Integer.MAX_VALUE) count++;
-        }
-        return count;
+    } 
+    nums[ptr] = nums[n];
+    return (n+1)-count;
     }
-
-   static int[] bubbleSort(int[] nums) {
-		    boolean swapped; 
-		   // Run the array for n-1 times.
-		   for (int i = 0; i < nums.length; i++) {
-		    swapped = false;
-		      // For each step, max item will come at last respective index.
-		      for (int j = 1; j < nums.length-i; j++) {
-		        if(nums[j] < nums[j-1]){
-                    
-		          int temp = nums[j];
-		          nums[j] = nums[j-1];
-		          nums[j-1] = temp;
-		          swapped = true;
-		        }
-		      }
-		      if(!swapped) {return nums;} 
-		   }
-		   return nums;
-  }
 }
