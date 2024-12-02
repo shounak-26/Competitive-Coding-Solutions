392f3b004f01adc48df6538c8e6f106ffb11fea1
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-12-02 19:54:44 +0530

    Time: 1 ms (99.88%), Space: 53.8 MB (5.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,23 +1,38 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> myMap = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            if(!myMap.containsKey(nums[i])){
-                myMap.put(nums[i],1);
-            }else{
-                myMap.put(nums[i], myMap.get(nums[i]) + 1);
+        // HashMap<Integer,Integer> myMap = new HashMap<>();
+        // for(int i = 0; i < nums.length; i++){
+        //     if(!myMap.containsKey(nums[i])){
+        //         myMap.put(nums[i],1);
+        //     }else{
+        //         myMap.put(nums[i], myMap.get(nums[i]) + 1);
+        //     }
+        // }
+
+        // int maxFrequency = 0;
+        // int maxKey = 0;
+
+        // for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
+        //     if (entry.getValue() > maxFrequency) {
+        //         maxFrequency = entry.getValue();
+        //         maxKey = entry.getKey();
+        //     }
+        // }
+        // return maxKey;
+
+        // Optimal solution using moon's algorithm
+        int count = 0;
+        int el = 0;
+        for (int i = 0; i < nums.length; i++) {
+        if(count == 0){
+            count = 1;
+            el = nums[i];
+        }else if(nums[i] == el){
+            count++;
+        }else{
+            count--;
             }
         }
-
-        int maxFrequency = 0;
-        int maxKey = 0;
-
-        for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
-            if (entry.getValue() > maxFrequency) {
-                maxFrequency = entry.getValue();
-                maxKey = entry.getKey();
-            }
-        }
-        return maxKey;
+        return el;
     }
 }
