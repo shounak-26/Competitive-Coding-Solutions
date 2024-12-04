d78c93ee25edf19065c9934c67a102d6d5f04218
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-12-04 19:55:46 +0530

    Time: 3 ms (100%), Space: 76.8 MB (64.87%) - LeetHub

---
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -0,0 +1,44 @@
+class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//     int positive = 0;
+//     int negative = 1;
+//     int n = nums.length;
+//     for (int i = 0; i < n; i++) {
+//       if (nums[i] > 0) {
+//         if ((positive % 2) == (i % 2) && positive < n) {
+//           positive += 2;
+//         } else {
+//           int temp = nums[i];
+//           nums[i] = nums[positive];
+//           nums[positive] = temp;
+//         }
+//       } else {
+//         if ((negative % 2) == (i % 2) && negative < n) {
+//           negative += 2;
+//         } else {
+//           int temp = nums[i];
+//           nums[i] = nums[negative];
+//           nums[negative] = temp;
+//         }
+//       }
+//     }
+//     return nums;
+//   }
+
+public int[] rearrangeArray(int[] nums) {
+    int positive = 0;
+    int negative = 1;
+    int n = nums.length;
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++) {
+      if (nums[i] > 0) {
+        result[positive] = nums[i];
+        positive+=2;
+      } else {
+        result[negative] = nums[i];
+        negative+=2;
+      }
+    }
+    return result;
+  }
+}
