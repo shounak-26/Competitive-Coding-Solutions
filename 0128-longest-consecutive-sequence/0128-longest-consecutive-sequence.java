f95d84d67a9e7d39882bae0172fc2f7ed165c3ef
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-12-17 23:24:32 +0530

    Time: 187 ms (34.15%), Space: 58.8 MB (75.24%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,21 +1,30 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-      int maxVal = 0;
-      if(nums.length == 0) return 0;
-      TreeSet<Integer> sortedSet = new TreeSet<>();  
-      for (int i : nums) {
-         sortedSet.add(i);
+    public  int longestConsecutive(int[] nums) {
+      TreeMap<Integer, Integer> myTreeMap = new TreeMap<>();
+
+      if(nums.length==0) return 0;
+
+      for (int i = 0; i < nums.length; i++) {
+         if (!myTreeMap.containsKey(nums[i])) {
+            myTreeMap.put(nums[i], 1);
+         } else {
+            myTreeMap.put(nums[i], myTreeMap.get(nums[i]) + 1);
+         }
       }
-      List<Integer> list = new ArrayList<>(sortedSet);
+
+      int maxVal = 0;
+      Integer firstKey = myTreeMap.firstKey();
+      Integer currentKey = myTreeMap.higherKey(firstKey);
       int temp = 0;
-      for (int i = 1; i < list.size(); i++) {
-         if((list.get(i) - list.get(i-1)) <= 1){
+      for (int i = 1; i < myTreeMap.size(); i++) {
+         Integer prevKey = myTreeMap.lowerKey(currentKey);
+         if ((currentKey - prevKey) == 1) {
             temp++;
             maxVal = Math.max(maxVal, temp);
-         }else{
-            maxVal = Math.max(maxVal, temp);
+         } else {
             temp = 0;
          }
+         currentKey = myTreeMap.higherKey(currentKey);
       }
       return maxVal+1;
    }
