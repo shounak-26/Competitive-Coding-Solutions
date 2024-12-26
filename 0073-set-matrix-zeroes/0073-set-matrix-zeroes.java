2e8da76161ee5e19ba0acd9bc5cc2b967bdff237
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-12-26 15:58:27 +0530

    Time: 1 ms (74.89%), Space: 45.6 MB (66.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,28 +3,28 @@ class Solution {
         // Brute Force Approch
         int m = matrix.length;
     int n = matrix[0].length;
+    boolean[][] result = new boolean[m][n];
     for(int i = 0; i < m; i++){
       for(int j = 0; j < n; j++){
         if(matrix[i][j] == 0){
+          result[i][j] = true;
+        }else{
+          result[i][j] = false;
+        }
+      }
+    }
+
+    for(int i = 0; i < m; i++){
+      for(int j = 0; j < n; j++){
+        if(result[i][j] == true){
           for(int k = 0; k < m; k++){
-            if(matrix[k][j] != 0){
-              matrix[k][j] = Integer.MAX_VALUE+3;
-            }
+              matrix[k][j] = 0;      
           }
           for(int f = 0; f < n; f++){
-            if(matrix[i][f] != 0){
-              matrix[i][f] = Integer.MAX_VALUE+3;
-            }
+              matrix[i][f] = 0;
           }
         }
       }
     }
-    for(int i = 0; i < m; i++){
-      for(int j = 0; j < n; j++){
-        if(matrix[i][j] == Integer.MAX_VALUE+3){
-          matrix[i][j] = 0;
-        }
-      }
-    } 
     }
 }
