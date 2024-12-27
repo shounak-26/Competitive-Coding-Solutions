644adf96270cf86ae4bfe4e93d8298abed0c4faf
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-12-27 15:36:25 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.4%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,18 +1,20 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int[][] result = new int[n][n];
-    
         for(int i=0; i<n; i++){
-            int temp = n-1-i;
-            for(int j=0; j<n; j++){
-                result[j][temp] = matrix[i][j];
+            for(int j=i+1; j<n; j++){
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
             }
         }
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = result[i][j];
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
             }
-        }
+        }    
     }
 }
