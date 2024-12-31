c47c29b0c533f8d269cb1d8beb77e3955633e7c5
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2024-12-31 14:06:12 +0530

    Time: 1 ms (86.43%), Space: 42.3 MB (19.2%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,18 +1,9 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>() {{
-            add(1);
-        }});
-        if(numRows < 2) return result;
-        result.add(new ArrayList<>() {{
-            add(1);
-            add(1);
-        }});
-        System.out.println(result);
-        for (int i = 2; i < numRows; i++) {
+        for (int i = 0; i < numRows; i++) {
             List<Integer> list = new ArrayList<>();
-            for (int j = 0; j < i+1; j++) {
+            for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
                     list.add(1);
                 } else {
