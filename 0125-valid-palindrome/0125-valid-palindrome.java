35298c96b8a0144c4856781e5351f7f8500e42c6
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2025-02-27 18:46:24 +0530

    Time: 139 ms (5.13%), Space: 46.1 MB (10.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,16 @@
 class Solution {
-    public static boolean isPalindrome(String s) {
+    public boolean isPalindrome(String s) {
         s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        StringBuilder sb = new StringBuilder(s);
-        System.out.println(sb + " " + sb.reverse());
-        if((sb+"").equals(sb.reverse()+""))
-        return true;
-        else return false;
+        int right = s.length()-1;
+        String reverse = "";
+        while(right >= 0){
+            reverse =  reverse + s.charAt(right);
+            right--;
+        }
+        System.out.println(s);
+        System.out.println(reverse);
+        if(s.equals(reverse))return true;
+        else return  false;
+        
     }
 }
