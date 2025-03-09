dd1b0940f67eb4bca27f2ddbb44184c73c045776
Shounak Purnapatre <93966554+shounak-26@users.noreply.github.com>
2025-03-09 11:51:24 +0530

    Time: 6 ms (81.89%), Space: 45.4 MB (46.33%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,37 +1,13 @@
 class Solution {
     public int firstUniqChar(String s) {
         // Approch 1
-        // int[] charArray = new int[26];
-        // for(char i : s.toCharArray()){
-        //     charArray[i - 'a']++;
-        // }
-
-        // for(int i = 0; i < s.length(); i++){
-        //     if(charArray[s.charAt(i) - 'a'] == 1){
-        //         return i;
-        //     }
-        // }
-        // return -1;
-
-        // Approch: 2 less better
-        // for(int i = 0; i < s.length(); i++){
-        //     if(s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))){
-        //         return i;
-        //     }
-        // }
-        // return -1;
-
-        // Approch 3: 
-        HashMap<Character,Integer> myMap = new HashMap<>();
-        for(int i = 0; i < s.length(); i++){
-            if(!myMap.containsKey(s.charAt(i))){
-                myMap.put(s.charAt(i),1);
-            }else{
-                myMap.put(s.charAt(i), myMap.get(s.charAt(i))+1);
-            }
+        int[] charArray = new int[26];
+        for(char i : s.toCharArray()){
+            charArray[i - 'a']++;
         }
+
         for(int i = 0; i < s.length(); i++){
-            if(myMap.get(s.charAt(i)) == 1){
+            if(charArray[s.charAt(i) - 'a'] == 1){
                 return i;
             }
         }
